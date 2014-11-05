a6f418fb75ca220e9e421ecdf1120b34c7b722ba
harrisyip <yip.harris@gmail.com>
2014-11-04 22:45:59 -0500

    Add react.js to the project and start demo app

---
+++ jsx/tutorial.jsx
@@ -0,0 +1,68 @@
+var Comment = React.createClass({
+	render: function(){
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				{this.props.children}
+			</div>	
+		)
+	}
+})
+var CommentList = React.createClass({
+	render: function(){
+		var commentNodes = this.props.data.map(function (comment){
+			return (
+				<Comment author={comment.author}>{comment.text}</Comment>
+			);
+		});
+		return (
+			<div className="commentList">
+				{commentNodes}
+			</div>
+		);
+	}
+});
+
+var CommentForm = React.createClass({
+	render: function(){
+		return (
+			<div className="commentForm">
+				Hello, I am commentForm
+			</div>	
+		);
+	}
+});
+
+var CommentBox = React.createClass({
+	getInitialState: function(){
+		return {data:[]};
+	},
+	componentDidMount: function(){
+		$.ajax({
+			url: this.props.url,
+			dataType: 'json',
+			success: function(data){
+				this.setState({data : data});
+			}.bind(this),
+			error: function(xhr, status, err){
+				console.error(this.props.url, status, err.toString());
+			}.bind(this)
+		});
+	},
+	render: function(){
+		return (
+			<div className="commentBox">
+				<h1>Comments</h1>
+				<CommentList data= {this.state.data}/>
+				<CommentForm />
+			</div>
+		);
+	}
+});
+
+React.render(
+	<CommentBox url="comments.json"/>,
+	document.getElementById('content')
+);
